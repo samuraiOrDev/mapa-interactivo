df101e8a5f7da7102c44790774ba04429f7d3ff5
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-08-18 20:28:22 +0200

    second commit

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -14,14 +14,16 @@ export const SearchBar = () => {
     }, 500);
   };
   return (
-    <div className="search-container">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Buscar lugar..."
-        onChange={onQueryChanged}
-      />
-      <SearchResults />
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Buscar lugar..."
+          onChange={onQueryChanged}
+        />
+        <SearchResults />
+      </div>
+    </>
   );
 };
--- src/utils/getUserLocation.ts
+++ src/utils/getUserLocation.ts
@@ -2,7 +2,11 @@ export const getUserLocation = async (): Promise<[number, number]> => {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
       ({ coords }) => {
-        resolve([coords.latitude, coords.latitude]);
+        console.log({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
+        resolve([coords.longitude, coords.latitude]);
       },
       (err) => {
         alert("No se puede obtener la geolocalización");
